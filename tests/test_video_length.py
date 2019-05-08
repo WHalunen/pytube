bdf5c9093990ce855c1153bc12c25575cc300b6d
stanley francois <stanleyson7@gmail.com>
2019-05-07 23:42:55 -0400

    Update test_video_length.py
    EXPLAINATION OF THE FIRST TWO LINES OF CODE

--- tests/test_video_length.py
+++ tests/test_video_length.py
@@ -1,18 +1,14 @@
 #length is working fine
 
-from pytube import YouTube
+from pytube import YouTube         # LET 'S  START BY IMPORTING THE YOUTUBE CLASS
 import math
 
 #ENTER A YOUTUBE VIDEO 'URL
 url = 'https://www.youtube.com/watch?v=kHLHSlExFis'
-yt = YouTube(url)
+yt = YouTube(url)  # HERE WE HAVE A YOUTUBE OBJECT THAT WE NAMED YT
 print('video description: ', yt.description)
-#print('rating', yt.rating)
 length=yt.length
 
-
-
-#print('views', yt.views)
 length_int=int(length)
 length_min=math.floor(length_int / 60)
 length_seconds=length_int % 60
